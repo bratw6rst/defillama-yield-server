0e89c7920fa8cf549daf312f4e23267f574ccf74
bratw6rst <bratw6rst@proton.me>
2026-06-12 11:31:50 +1000

    origami-finance: get tvl usd from onchain

--- src/adaptors/afterTests.js
+++ src/adaptors/afterTests.js
@@ -2,9 +2,12 @@ module.exports = function () {
   console.log(`\nNb of pools: ${global.apy.length}\n `);
   if (process.env.CI !== undefined) {
     console.log('\nSample pools:');
-    console.table(global.apy.slice(0, 10));
+    console.table(global.apy.sort((a, b) => a.pool.localeCompare(b.pool)));
   } else {
-    console.log('\nSample pools:', global.apy.slice(0, 10));
+    console.log(
+      '\nSample pools:',
+      global.apy.sort((a, b) => a.pool.localeCompare(b.pool))
+    );
   }
   if (global.apy.some((p) => p.tvlUsd < 10e3)) {
     console.log(
